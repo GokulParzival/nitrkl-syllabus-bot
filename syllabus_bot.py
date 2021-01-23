4eb5de47c18aa133586420c1aa0f2106c8afce6a
Gokulakannan <41119095+GokulParzival@users.noreply.github.com>
2021-01-23 20:13:39 +0530

    EE and ME was added

--- syllabus_bot.py
+++ syllabus_bot.py
@@ -5,7 +5,7 @@ def syllabus_finder(s_code):
     url = "https://sheetdb.io/api/v1/q1qqa1kg7bit1"
     json_syllabus = requests.get(url).json()
     size = len(json_syllabus)
-    print("URL working fine")
+    #print("URL working fine")
     for i in range(size):
         dictionary = json_syllabus[i]
         subject = dictionary["Code"]
